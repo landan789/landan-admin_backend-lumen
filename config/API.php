019173ac12e5892ab1d803c754a26148429cedca
landanhsu <landanhsu.aplusaccelinc@gmail.com>
2018-09-28 14:35:00 +0800

    [feature/const]
    config

---
+++ config/API.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2018/9/28
+ * Time: 14:11
+ */return [
+    'JWT' => [
+        'EXPIRES'  => 60 * 60 * 1000,
+        'SUBJECT'  => 'support@jinku.com',
+        'ISSUER'   => 'support@jinku.com',
+        'AUDIENCE' => 'jinku.com',
+        'SECRET'   => 'ilovejinku'
+    ]
+];
