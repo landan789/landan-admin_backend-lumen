2a6275dcb04df3b714ef985225c319cc93ac6244
landanhsu <landanhsu.aplusaccelinc@gmail.com>
2018-09-30 02:02:30 +0800

    [feature/composer]
    autoload

---
+++ helpers/Jwt.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 2018/9/29
+ * Time: 23:42
+ */
+
+namespace Aplusaccelinc\Helpers;
+
+use \Firebase\JWT\JWT as _JWT;
+
+class Jwt
+{
+    public function encode($userId, $customerId, $expires)
+    {
+        $payload = [
+            'sub' => config('API.JWT.SUBJECT'),
+            'iss' => config('API.JWT.ISSUER'),
+            'adu' => config('API.JWT.AUDIENCE'),
+            'exp' => $expires && 0 < $expires ? $expires : time() + config('API.JWT.EXPIRES'),
+            'iat' => time(),
+            'uid' => $userId ? $userId : '',
+            'cid' => $customerId ? $customerId : ''
+        ];
+
+        $jwt = _JWT::encode($payload);
+
+        return $jwt;
+    }
+
+    public function decode()
+    {
+
+    }
+}
