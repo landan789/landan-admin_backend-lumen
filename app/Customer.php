1660af217adabab9e94cf6a1b2c528671908d693
landan789 <45192807+landan789@users.noreply.github.com>
2019-01-02 20:05:25 +0800

    Update Customer.php

--- app/Customer.php
+++ app/Customer.php
@@ -16,6 +16,8 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 class Customer extends Model {
     protected $table = 'customer';
     protected $primaryKey = 'customer_id';
+    const CREATED_AT = 'created_time';
+    const UPDATED_AT = 'update_time';
     /**
      * The attributes that are mass assignable.
      *
