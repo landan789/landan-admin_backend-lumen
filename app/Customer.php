6cfd6a2e7bae32cab5b0730f2def22fa4d1f6dc3
landan789 <45192807+landan789@users.noreply.github.com>
2019-01-11 13:14:45 +0800

    Update Customer.php

--- app/Customer.php
+++ app/Customer.php
@@ -9,7 +9,7 @@
 namespace App;
 
 use Laravel\Lumen\Auth\Authorizable;
-use Illuminate\Database\Eloquent\Model;
+use Core\Model;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
