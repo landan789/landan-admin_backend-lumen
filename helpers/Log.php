9f666df1f00e3def916fbca22c72cbd7b69fd2ce
LANDAN <landanhsu@bpit888.com>
2019-02-23 22:50:33 +0800

    Modfified, refactor, 命名微调

--- helpers/Log.php
+++ helpers/Log.php
@@ -13,7 +13,7 @@ use function Aplusaccelinc\Functions\clientIP;
 
 class Log {
     public static $oRequestAll = null;
-    public static $oRequstServer = null;
+    public static $oRequestServer = null;
 
     public static $channel = 'BACKEND';
     public static function start ($oRequest, $sJwt) {
@@ -21,17 +21,17 @@ class Log {
         self::$oRequstAll = $oRequest->all();
         self::$oRequstServer = $oRequest->server();
         _Log::channel(self::$channel)
-             ->info(clientIP() . ' ' . $oRequest->method() . ' ' . json_encode(self::$oRequstServer) . ' ' . $sJwt .' ' . json_encode(self::$oRequestAll));
+             ->info(clientIP() . ' ' . $oRequest->method() . ' ' . json_encode(self::$oRequestServer) . ' ' . $sJwt .' ' . json_encode(self::$oRequestAll));
     }
 
     public static function succeed ($oRequest, $sJwt) {
         _Log::channel(self::$channel)
-            ->critical(clientIP() . ' ' . $oRequest->method() . ' ' . json_encode(self::$oRequstServer) . ' ' . $sJwt .' ' . json_encode(self::$oRequestAll));
+            ->critical(clientIP() . ' ' . $oRequest->method() . ' ' . json_encode(self::$oRequestServer) . ' ' . $sJwt .' ' . json_encode(self::$oRequestAll));
 
     }
 
     public static function  fail ($oRequest, $sJwt) {
          _Log::channel(self::$channel)
-             ->error(clientIP() . ' ' . $oRequest->method() . ' ' . json_encode(self::$oRequstServer) . ' ' . $sJwt .' ' . json_encode(self::$oRequestAll));
+             ->error(clientIP() . ' ' . $oRequest->method() . ' ' . json_encode(self::$oRequestServer) . ' ' . $sJwt .' ' . json_encode(self::$oRequestAll));
     }
 }
