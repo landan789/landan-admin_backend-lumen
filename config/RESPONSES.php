2e6c4a6ca045f78b612aa457912e79e9b6e26534
LANDAN <landanhsu@bpit888.com>
2019-02-23 20:40:01 +0800

    Modfified, STATUS -> RESULT

--- config/RESPONSES.php
+++ config/RESPONSES.php
@@ -7,48 +7,62 @@
  *
  */return [
     'SUCCEED_TO_SHOW_LOTTERY' => [
+        'STATUS' => 200,
         'RESULT' => 1,
         'CODE' => 0,
         'MESSAGE' => 'succeed to show lottery'
     ],
     'SUCCEED_TO_SHOW_ISSUE' => [
+        'STATUS' => 200,
         'RESULT' => 1,
         'CODE' => 0,
         'MESSAGE' => 'succeed to show issue'
     ],
 
     'IS_UNKNOWN_ERROR' => [
+        'STATUS' => 500,
         'RESULT' => -1,
         'CODE' => -0.01,
         'MESSAGE' => 'is unknown error'
     ],
 
     'REQUEST_UNDEFINED_PATH' => [
+        'STATUS' => 501,
         'RESULT' => -1,
         'CODE' => -0.02,
         'MESSAGE' => 'request undefined path'
     ],
 
     'FAIL_TO_SHOW_LOTTERY' => [
+        'STATUS' => 500,
         'RESULT' => -1,
         'CODE' => -1.01,
         'MESSAGE' => 'fail to show lottery'
     ],
 
 
-    'JWT_IS_NOT_PERMITTED' => [
-        'CODE' => -4,
-        'MESSAGE' => 'jwt is not permitted'
+    'JWT_IS_FORBIDDEN' => [
+        'STATUS' => 403,
+        'RESULT' => -1,
+        'CODE' => -0.01,
+        'MESSAGE' => 'is forbidden'
     ],
-    'JWT_HAS_EXPIRED' => [
+    'JWT_IS_EXPIRED' => [
+        'STATUS' => 403,
+        'RESULT' => -1,
         'CODE' => -3,
-        'MESSAGE' => 'jwt has expired'
+        'MESSAGE' => 'is expired'
     ],
-    'JWT_IS_NOT_AUTHORIZED' => [
-        'CODE' => -2,
-        'MESSAGE' => 'jwt is not authorized'
+
+    'JWT_IS_UNAUTHORIZED' => [
+        'STATUS' => 401,
+        'RESULT' => -1,
+        'CODE' => -0.02,
+        'MESSAGE' => 'jwt is authorized'
     ],
     'JWT_IS_EMPTY' => [
+        'STATUS' => 401,
+        'RESULT' => -1,
         'CODE' => -1,
         'MESSAGE' => 'jwt is empty'
     ],
